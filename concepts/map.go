788a0d376daf20163580baffe99deb6cca753f66
Eduard Castro <eduard.cas07@gmail.com>
2020-08-26 18:18:03 -0500

    Add maps example

---
+++ concepts/map.go
@@ -0,0 +1,17 @@
+package map
+
+import (
+	"fmt"
+)
+
+func main() {
+	m := map[string]int{"foo": 42}
+	fmt.Println(m)
+	fmt.Println(m["foo"])
+	
+	m["foo"] = 27
+	fmt.Println(m)
+	
+	delete(m , "foo")
+	fmt.Println(m)
+}
