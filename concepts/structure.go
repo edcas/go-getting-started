bbeff1253aa894f224fd3d7597e11b3346a8d314
Eduard Castro <eduard.cas07@gmail.com>
2020-08-26 18:18:18 -0500

    Add struct example

---
+++ concepts/structure.go
@@ -0,0 +1,20 @@
+package structure
+
+import "fmt"
+
+func main() {
+	type user struct {
+		ID        int
+		FirstName string
+		LastName  string
+	}
+
+	var u user
+	u.ID = 1
+	u.FirstName = "Arthur"
+	u.LastName = "Dent"
+	fmt.Println(u)
+
+	u2 := user{ID: 1, FirstName: "Arthur", LastName: "Dent"}
+	fmt.Println(u2)
+}
