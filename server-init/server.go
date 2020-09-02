e76535f9b75114b4e1facc4b4104a35c4322e614
Eduard Castro <eduard.cas07@gmail.com>
2020-09-01 19:01:23 -0500

    Add config server - Server Web example

---
+++ server-init/server.go
@@ -0,0 +1,27 @@
+package main
+
+import "net/http"
+
+type Server struct {
+	port string
+	router *Router
+}
+
+func NewServer(port string) *Server {
+	return &Server{
+		port: port,
+		router: NewRouter(),
+	}
+}
+
+func (server *Server) Listen() error  {
+	http.Handle("/", server.router)
+
+	err := http.ListenAndServe(server.port, nil)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
